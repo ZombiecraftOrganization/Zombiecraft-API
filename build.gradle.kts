fd1e7618dd4a7ef8b8dfbf1549274d83501075a7
Stephcraft <contact@stephcraft.net>
2023-06-27 11:26:39 -0400

    1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ val gpr: DependencyHandler.(String, String?) -> Any by extra
 fun DependencyHandler.projectOrGithubPackage(path: String, configuration: String? = null): Any = gpr(path, configuration)
 
 dependencies {
-    compileOnly(projectOrGithubPackage(":zombiecraft-utility", "shadow"))
+    compileOnly(projectOrGithubPackage(":zombiecraft-utility"))
 
     // spigot
-    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
 
     // test
     testImplementation("junit:junit:4.13.2")
